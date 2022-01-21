0196d683a09ec198853bee6860caea5ffa06e260
Niklas Blomberg <blomberg.niklas@gmail.com>
2022-01-21 23:21:05 +0100

    Removed postinstall. Adding dist/

---
+++ dist/algolia.providers.d.ts
@@ -0,0 +1,3 @@
+import { Provider } from '@nestjs/common';
+import { AlgoliaIndexDefinition } from './interfaces';
+export declare const createAlgoliaIndexProviders: (indexData: AlgoliaIndexDefinition[]) => Provider[];
